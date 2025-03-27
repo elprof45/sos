60db105c989aac00dc0f011ab12be56f74e24e65
AGBO Martin <letcode.dev@gmail.com>
2025-03-27 12:12:48 +0000

    Update page.tsx

--- app/view/sos/[userID]/page.tsx
+++ app/view/sos/[userID]/page.tsx
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { userIdParmSchema } from "@/lib/zod";
 import ButtomClick from "@/components/common/button";
 import { revalidatePath } from "next/cache";
+import Image from "next/image";
 
 type Props = {
   params: {
@@ -78,7 +79,7 @@ const PatientInfo = async ({ params }: Props) => {
           <div className="flex flex-col justify-center items-center">
             <div className="avatar placeholder relative z-30 ">
               <div className="w-24 bg-neutral text-neutral-content rounded-full mx-auto border border-white border-x-4 border-y-4 shadow-lg">
-                {user.profile ? <img className="w-32 h-32 "
+                {user.profile ? <Image className="w-32 h-32 "
                   src={user.profile} alt="Photo de Profil" /> : <span className="text-title-xxl2 font-bold space-y-4 ">{user.firstName[0].toUpperCase()} </span>}
               </div>
             </div>
@@ -116,7 +117,7 @@ const PatientInfo = async ({ params }: Props) => {
         </div>
 
         <div className="mt-4">
-          <p className="text-xl font-semibold space-y-4 text-center">Numéros d'urgence :</p>
+          <p className="text-xl font-semibold space-y-4 text-center">Numéros d&apos urgence :</p>
           <div className="flex justify-center my-4">
             <a href="tel:118">
               <button className="bg-blue-500 text-white  font-semibold py-2 px-4 rounded-lg w-full">
@@ -134,7 +135,7 @@ const PatientInfo = async ({ params }: Props) => {
           <div className="flex justify-center my-4">
             <a href="tel:999">
               <button className="bg-blue-500 text-white  font-semibold py-2 px-4 rounded-lg w-full">
-                Service d'Urgence Médical
+                Service d&apos Urgence Médical
               </button>
             </a>
           </div>
